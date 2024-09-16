8c7fdc62918278099d1e205e20563d96c406ea11
Marko Goronjic <marko.goronjic@vyking.com>
2024-09-16 12:50:33 +0200

    Array & Object Literals

--- dev/index.js
+++ dev/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log('hello, pirates!');
+//--------
+// arrays
+//--------
+let names = ['Luffy', 'Zoro', 'Usopp'];
+let ages = [17, 19, 17];
+names.push('Nami');
+ages.push(18);
+let multi = [1, true, 'hello'];
+const m = multi[0];
+//----------------------------
+// type inference with arrays
+//----------------------------
+let fruits = ['gum-gum', 'flower-flower', 'ice-ice', 'human-human'];
+fruits.push('revive-revive');
+const f = fruits[3];
+console.log(f);
+let things = [1, true, 'hello'];
+const t = things[0];
+//-----------------
+// object literals
+//-----------------
+const user = {
+    firstName: 'Luffy',
+    age: 17,
+    id: 1
+};
+user.firstName = 'Zoro';
+user.id = 2;
+//----------------------------
+// type inference object literals
+//----------------------------
+let person = {
+    name: 'Luffy',
+    score: 35
+};
+person.name = 'Franky';
+person.score = 40;
+const score = person.score;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-console.log('hello, pirates!');
+//--------
+// arrays
+//--------
+
+let names: string[] = ['Luffy', 'Zoro', 'Usopp'];
+let ages: number[] = [17, 19, 17];
+
+names.push('Nami')
+ages.push(18)
+
+let multi: (string | number | boolean)[] = [1, true, 'hello']
+
+const m: string | number | boolean = multi[0];
+
+
+//----------------------------
+// type inference with arrays
+//----------------------------
+
+let fruits = ['gum-gum', 'flower-flower', 'ice-ice', 'human-human'];
+
+fruits.push('revive-revive')
+
+const f = fruits[3]
+
+console.log(f);
+
+let things: (string | number | boolean)[] = [1, true, 'hello']
+
+const t: string | number | boolean = things[0];
+
+
+//-----------------
+// object literals
+//-----------------
+
+const user: {firstName: string, age: number, id: number} = {
+    firstName: 'Luffy',
+    age: 17,
+    id: 1
+};
+
+user.firstName = 'Zoro'
+user.id = 2
+
+//----------------------------
+// type inference object literals
+//----------------------------
+
+let person = {
+    name: 'Luffy',
+    score: 35
+}
+
+person.name = 'Franky'
+person.score = 40
+
+const score = person.score
